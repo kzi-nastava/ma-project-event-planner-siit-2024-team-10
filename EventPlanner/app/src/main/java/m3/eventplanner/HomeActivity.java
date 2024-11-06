1cc1a48e5bc6b18f0dbd48466b9031ef23aeadbc
Marina Ivanovic <ivanovic.sv6.2022@uns.ac.rs>
2024-11-06 21:17:36 +0100

    finished main page design

--- EventPlanner/app/src/main/java/m3/eventplanner/HomeActivity.java
+++ EventPlanner/app/src/main/java/m3/eventplanner/HomeActivity.java
@@ -1,18 +1,8 @@
 package m3.eventplanner;
 
 import android.os.Bundle;
-import android.widget.ListView;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import java.util.ArrayList;
-import java.util.Collections;
-
-import m3.eventplanner.adapters.EventCardAdapter;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -20,12 +10,5 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
-
-        ListView eventListView = findViewById(android.R.id.list);
-
-        ArrayList<String> events = new ArrayList<>(Collections.nCopies(5, "Event"));
-
-        EventCardAdapter adapter = new EventCardAdapter(this, R.layout.event_card, events);
-        eventListView.setAdapter(adapter);
     }
 }
