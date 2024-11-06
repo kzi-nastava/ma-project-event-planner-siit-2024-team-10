36a697603f44c9f49c1d9d7476c49f14dbc3291f
milicaradicc <milica.t.radic@gmail.com>
2024-11-06 12:56:46 +0100

    Add SplashScreenActivity

--- EventPlanner/app/src/main/java/m3/eventplanner/SplashScreenActivity.java
+++ EventPlanner/app/src/main/java/m3/eventplanner/SplashScreenActivity.java
@@ -1,5 +1,6 @@
 package m3.eventplanner;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,17 +9,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // requested five seconds wait until homescreen is shown
+        int SPLASH_TIME_OUT = 5000;
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
