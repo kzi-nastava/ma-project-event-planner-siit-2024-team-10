14d1f647f0ac8211d2f7177d46c145d734033f63
Mijat Krivokapić <mijat2004@gmail.com>
2024-11-06 18:55:05 +0100

    Add company register page

--- EventPlanner/app/src/main/java/m3/eventplanner/activities/RegisterCompanyActivity.java
+++ EventPlanner/app/src/main/java/m3/eventplanner/activities/RegisterCompanyActivity.java
@@ -1,8 +1,6 @@
 package m3.eventplanner.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,23 +10,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 import m3.eventplanner.R;
 
-public class RegisterPersonalActivity extends AppCompatActivity {
+public class RegisterCompanyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_register_personal);
+        setContentView(R.layout.activity_register_company);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        Button submitButton = findViewById(R.id.submit);
-        submitButton.setOnClickListener(v -> {
-            Intent intent=new Intent(RegisterPersonalActivity.this,RegisterCompanyActivity.class);
-            startActivity(intent);
         });
     }
 }
