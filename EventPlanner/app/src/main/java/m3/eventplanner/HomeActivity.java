360f59048dd5654e2cec396638035b0496875c6e
Marina Ivanovic <ivanovic.sv6.2022@uns.ac.rs>
2024-11-06 18:45:18 +0100

    xml connected to the activity

--- EventPlanner/app/src/main/java/m3/eventplanner/HomeActivity.java
+++ EventPlanner/app/src/main/java/m3/eventplanner/HomeActivity.java
@@ -1,4 +1,31 @@
 package m3.eventplanner;
 
-public class HomeActivity {
+import android.os.Bundle;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import m3.eventplanner.adapters.EventCardAdapter;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homescreen);
+
+        ListView eventListView = findViewById(android.R.id.list);
+
+        ArrayList<String> events = new ArrayList<>(Collections.nCopies(5, "Event"));
+
+        EventCardAdapter adapter = new EventCardAdapter(this, R.layout.event_card, events);
+        eventListView.setAdapter(adapter);
+    }
 }
