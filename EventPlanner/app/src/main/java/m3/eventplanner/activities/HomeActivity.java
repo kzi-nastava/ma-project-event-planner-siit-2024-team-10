dc6362f5d828e2a018008a684aedd4ef9d345153
Marina Ivanovic <ivanovic.sv6.2022@uns.ac.rs>
2024-11-06 21:31:25 +0100

    changed .gitignore and fixed imports

--- EventPlanner/app/src/main/java/m3/eventplanner/activities/HomeActivity.java
+++ EventPlanner/app/src/main/java/m3/eventplanner/activities/HomeActivity.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 import m3.eventplanner.R;
-import m3.eventplanner.adapters.EventCardAdapter;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -18,11 +17,5 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
 
-        ListView eventListView = findViewById(android.R.id.list);
-
-        ArrayList<String> events = new ArrayList<>(Collections.nCopies(5, "Event"));
-
-        EventCardAdapter adapter = new EventCardAdapter(this, R.layout.event_card, events);
-        eventListView.setAdapter(adapter);
     }
 }
