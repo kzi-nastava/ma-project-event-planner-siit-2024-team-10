fb28e2b022d0b59a72b13b9ec52af431f72ef81c
Marina Ivanovic <ivanovic.sv6.2022@uns.ac.rs>
2024-11-12 19:45:35 +0100

    completed event/offering tabs

---
+++ EventPlanner/app/src/main/java/m3/eventplanner/fragments/HomeScreenEvents.java
@@ -0,0 +1,19 @@
+package m3.eventplanner.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import m3.eventplanner.R;
+
+public class HomeScreenEvents extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home_screen_events, container, false);
+    }
+}
--- EventPlanner/app/src/main/java/m3/eventplanner/fragments/HomeScreenFragment.java
+++ EventPlanner/app/src/main/java/m3/eventplanner/fragments/HomeScreenFragment.java
@@ -5,36 +5,27 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
-import androidx.viewpager2.widget.ViewPager2;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
 import m3.eventplanner.R;
 
 public class HomeScreenFragment extends Fragment {
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager;
     public HomeScreenFragment() {
         // Required empty public constructor
     }
-    private TabLayout tabLayout;
-    private ViewPager2 viewPager;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_homescreen, container, false);
-    }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-    }
-}
         tabLayout = rootView.findViewById(R.id.homeScreenTab);
         viewPager = rootView.findViewById(R.id.homeScreenViewPager);
 
@@ -43,11 +34,9 @@ public class HomeScreenFragment extends Fragment {
             @Override
             public Fragment createFragment(int position) {
                 if (position == 0) {
-                    return null;
-                    //return new HomeScreenEvents();
+                    return new HomeScreenEvents();
                 } else {
-                    return null;
-                    //return new HomeScreenOfferings();
+                    return new HomeScreenOfferings();
                 }
             }
 
@@ -68,5 +57,9 @@ public class HomeScreenFragment extends Fragment {
 
         return rootView;
     }
-}
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
