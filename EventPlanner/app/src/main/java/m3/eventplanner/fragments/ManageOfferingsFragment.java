d27aceb698298f7d8d646e893a4ac247079c25f7
milicaradicc <milica.t.radic@gmail.com>
2024-11-13 15:00:16 +0100

    Convert manage offerings activity to fragment

---
+++ EventPlanner/app/src/main/java/m3/eventplanner/fragments/ManageOfferingsFragment.java
@@ -0,0 +1,26 @@
+package m3.eventplanner.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import m3.eventplanner.R;
+
+
+public class ManageOfferingsFragment extends Fragment {
+
+    public ManageOfferingsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_manage_offerings, container, false);
+    }
+}
