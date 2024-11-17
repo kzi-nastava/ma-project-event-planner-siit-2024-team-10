e548759cda6f050d6b8853b3d33d6adf2d78011a
milicaradicc <milica.t.radic@gmail.com>
2024-11-17 15:34:27 +0100

    Add fragment EditService

---
+++ EventPlanner/app/src/main/java/m3/eventplanner/fragments/EditOfferingFragment.java
@@ -0,0 +1,31 @@
+package m3.eventplanner.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import m3.eventplanner.R;
+
+public class EditOfferingFragment extends Fragment {
+
+    public EditOfferingFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit_offering, container, false);
+    }
+}
