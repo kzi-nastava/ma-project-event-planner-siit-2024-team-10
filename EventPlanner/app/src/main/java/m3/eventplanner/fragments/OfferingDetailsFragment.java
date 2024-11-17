b46da9ef9b7c6ed803484d765bac9a52b5840a2b
milicaradicc <milica.t.radic@gmail.com>
2024-11-17 17:40:05 +0100

    Enable opening edit service form

--- EventPlanner/app/src/main/java/m3/eventplanner/fragments/OfferingDetailsFragment.java
+++ EventPlanner/app/src/main/java/m3/eventplanner/fragments/OfferingDetailsFragment.java
@@ -1,13 +1,17 @@
 package m3.eventplanner.fragments;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import m3.eventplanner.R;
 import m3.eventplanner.models.Offering;
@@ -32,5 +36,31 @@ public class OfferingDetailsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        Button edit = view.findViewById(R.id.edit_offering_button);
+        edit.setOnClickListener(v -> {
+            NavController navController = NavHostFragment.findNavController(OfferingDetailsFragment.this);
+            navController.navigate(R.id.editOfferingFragment);
+        });
+
+        Button delete = view.findViewById(R.id.delete_offering_button);
+        delete.setOnClickListener(v -> showDeleteDialog());
+    }
+    private void showDeleteDialog() {
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.conformation_dialog, null);
+
+        Button btnCancel = dialogView.findViewById(R.id.btn_cancel);
+        Button btnOk = dialogView.findViewById(R.id.btn_ok);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        builder.setView(dialogView);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+        btnCancel.setOnClickListener(v -> dialog.dismiss());
+
+        btnOk.setOnClickListener(v -> dialog.dismiss());
     }
 }
