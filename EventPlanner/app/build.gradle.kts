856ca448cf9f494f3a6384a269f6184dea21cba4
Marina Ivanovic <ivanovic.sv6.2022@uns.ac.rs>
2024-12-15 14:06:01 +0100

    Add retrofit dependencies

--- EventPlanner/app/build.gradle.kts
+++ EventPlanner/app/build.gradle.kts
@@ -45,4 +45,7 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation("com.squareup.picasso:picasso:2.8")
+    implementation("com.squareup.retrofit2:retrofit:2.3.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.3.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:3.12.1")
 }
