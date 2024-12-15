e1a8aa8ed9003890398076929c36121303827139
Marina Ivanovic <ivanovic.sv6.2022@uns.ac.rs>
2024-12-15 15:16:54 +0100

    Add client utils

--- EventPlanner/app/src/main/java/m3/eventplanner/clients/ClientUtils.java
+++ EventPlanner/app/src/main/java/m3/eventplanner/clients/ClientUtils.java
@@ -14,4 +14,24 @@ public class ClientUtils {
     // FOR THIS TO WORK, YOU NEED TO CHANGE THE IP_ADDR IN THE local.properties FILE!!!
     // CHECK BY OPENING CMD PROMPT AND TYPE ipconfig, LOOK FOR WIRELESS CONNECTION
     public static final String SERVICE_API_PATH = "http://" + BuildConfig.IP_ADDR + ":8080/api/";
+
+    // This method is used for debugging, to see if the requirements and
+    // responses come and go as they are supposed to.
+    public static OkHttpClient test(){
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectTimeout(120, TimeUnit.SECONDS)
+                .readTimeout(120, TimeUnit.SECONDS)
+                .writeTimeout(120, TimeUnit.SECONDS)
+                .addInterceptor(interceptor).build();
+
+        return client;
+    }
+    public static Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(SERVICE_API_PATH)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(test())
+            .build();
 }
