2b0553d9aee77d7bc8e80f29717e866be1bd2893
Mijat Krivokapić <mijat2004@gmail.com>
2024-12-18 20:52:29 +0100

    Add dtos needed for registration

---
+++ EventPlanner/app/src/main/java/m3/eventplanner/models/CreateLocationDTO.java
@@ -0,0 +1,43 @@
+package m3.eventplanner.models;
+
+public class CreateLocationDTO {
+    private String city;
+    private String country;
+    private String street;
+    private String houseNumber;
+
+    public CreateLocationDTO() {
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(String houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+}
