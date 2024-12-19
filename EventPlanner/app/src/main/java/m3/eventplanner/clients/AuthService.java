4d5c492975d5a73b940741865e993509ca7b4ccf
Mijat Krivokapić <mijat2004@gmail.com>
2024-12-19 13:06:51 +0100

    Implement register service method

--- EventPlanner/app/src/main/java/m3/eventplanner/clients/AuthService.java
+++ EventPlanner/app/src/main/java/m3/eventplanner/clients/AuthService.java
@@ -2,11 +2,16 @@ package m3.eventplanner.clients;
 
 import m3.eventplanner.models.LoginRequestDTO;
 import m3.eventplanner.models.LoginResponseDTO;
+import m3.eventplanner.models.RegisterDTO;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 public interface AuthService {
     @POST("auth/login")
     Call<LoginResponseDTO> login(@Body LoginRequestDTO loginRequest);
+
+    @POST("auth/register")
+    Call<RegisterDTO> register(@Body RegisterDTO registerDTO, @Query("roleUpgrade") boolean roleUpgrade);
 }
