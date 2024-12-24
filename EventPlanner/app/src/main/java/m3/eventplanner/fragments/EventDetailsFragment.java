50ee36229e5ca8cbb02406c2757ded65305b2c67
Mijat Krivokapić <mijat2004@gmail.com>
2024-12-24 10:05:49 +0100

    Implement adding and removing event from favourites

--- EventPlanner/app/src/main/java/m3/eventplanner/fragments/EventDetailsFragment.java
+++ EventPlanner/app/src/main/java/m3/eventplanner/fragments/EventDetailsFragment.java
@@ -24,7 +24,9 @@ import java.util.List;
 import m3.eventplanner.R;
 import m3.eventplanner.adapters.AgendaItemListAdapter;
 import m3.eventplanner.adapters.EventListAdapter;
+import m3.eventplanner.auth.TokenManager;
 import m3.eventplanner.clients.ClientUtils;
+import m3.eventplanner.models.AddFavouriteEventDTO;
 import m3.eventplanner.models.AgendaItem;
 import m3.eventplanner.models.Event;
 import m3.eventplanner.models.GetAgendaItemDTO;
@@ -40,16 +42,19 @@ public class EventDetailsFragment extends Fragment {
     private RecyclerView agendaRecyclerView;
     private ImageView eventOrganizerProfilePhoto;
     private TextView eventOrganizerName, eventOrganizerLocation, eventOrganizerEmail, eventOrganizerPhone;
+    private ImageButton favouriteButton;
 
     private int eventId;
     private GetEventDTO event;
     private Collection<GetAgendaItemDTO> agenda;
     private ClientUtils clientUtils;
+    private boolean isFavourite = false;
 
 
     public EventDetailsFragment() {
         // Required empty public constructor
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -59,20 +64,54 @@ public class EventDetailsFragment extends Fragment {
 
         InitializeUIComponents(view);
 
+        favouriteButton.setOnClickListener(v -> {
+            int accountId=new TokenManager(requireContext()).getAccountId();
+            if(isFavourite){
+                clientUtils.getAccountService().removeEventFromFavourites(accountId, eventId).enqueue(new Callback<Void>() {
+                    @Override
+                    public void onResponse(Call<Void> call, Response<Void> response) {
+                        isFavourite=!isFavourite;
+                        RefreshFavouriteButton();
+                    }
+
+                    @Override
+                    public void onFailure(Call<Void> call, Throwable t) {
+                        // Handle failure
+                    }
+                });
+            }
+            else{
+                clientUtils.getAccountService().addEventToFavourites(accountId, new AddFavouriteEventDTO(eventId)).enqueue(new Callback<Void>() {
+                    @Override
+                    public void onResponse(Call<Void> call, Response<Void> response) {
+                        isFavourite=!isFavourite;
+                        RefreshFavouriteButton();
+                    }
+
+                    @Override
+                    public void onFailure(Call<Void> call, Throwable t) {
+                        // Handle failure
+                    }
+                });
+            }
+
+        });
+
         if (getArguments() != null) {
             eventId = getArguments().getInt("selectedEventId");
             Call<GetEventDTO> eventCall = clientUtils.getEventService().getEvent(eventId);
             eventCall.enqueue(new Callback<GetEventDTO>() {
                 @Override
                 public void onResponse(Call<GetEventDTO> call, Response<GetEventDTO> response) {
-                    if (response.isSuccessful() && response.body() != null){
-                        event=response.body();
+                    if (response.isSuccessful() && response.body() != null) {
+                        event = response.body();
                         PopulateEventDetails();
-                    } else{
+                    } else {
 
                         return;
                     }
                 }
+
                 @Override
                 public void onFailure(Call<GetEventDTO> call, Throwable t) {
                     Log.d("REZ", t.getMessage() != null ? t.getMessage() : "error");
@@ -83,15 +122,34 @@ public class EventDetailsFragment extends Fragment {
             agendaCall.enqueue(new Callback<Collection<GetAgendaItemDTO>>() {
                 @Override
                 public void onResponse(Call<Collection<GetAgendaItemDTO>> call, Response<Collection<GetAgendaItemDTO>> response) {
-                    if (response.isSuccessful() && response.body() != null){
+                    if (response.isSuccessful() && response.body() != null) {
                         agenda = response.body();
                         PopulateAgenda();
-                    } else{
-                        String a="aaaa";
+                    } else {
+                        return;
                     }
                 }
+
                 @Override
                 public void onFailure(Call<Collection<GetAgendaItemDTO>> call, Throwable t) {
+                    Log.d("REZ", t.getMessage() != null ? t.getMessage() : "error");
+                }
+            });
+
+            Call<Collection<GetEventDTO>> favouritesCall = clientUtils.getAccountService().getFavouriteEvents(new TokenManager(requireContext()).getAccountId());
+            favouritesCall.enqueue(new Callback<Collection<GetEventDTO>>() {
+                @Override
+                public void onResponse(Call<Collection<GetEventDTO>> call, Response<Collection<GetEventDTO>> response) {
+                    if (response.isSuccessful() && response.body() != null) {
+                        isFavourite = response.body().stream().anyMatch(event -> event.getId() == eventId);
+                        RefreshFavouriteButton();
+                    } else {
+                        return;
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<Collection<GetEventDTO>> call, Throwable t) {
                     Log.d("REZ", t.getMessage() != null ? t.getMessage() : "error");
                 }
             });
@@ -99,7 +157,7 @@ public class EventDetailsFragment extends Fragment {
         return view;
     }
 
-    private void InitializeUIComponents(View view){
+    private void InitializeUIComponents(View view) {
         eventName = view.findViewById(R.id.eventName);
         eventType = view.findViewById(R.id.eventType);
         eventDescription = view.findViewById(R.id.eventDescription);
@@ -111,9 +169,10 @@ public class EventDetailsFragment extends Fragment {
         eventOrganizerLocation = view.findViewById(R.id.eventOrganizerLocation);
         eventOrganizerEmail = view.findViewById(R.id.eventOrganizerEmail);
         eventOrganizerPhone = view.findViewById(R.id.eventOrganizerPhone);
+        favouriteButton = view.findViewById(R.id.favourite_button);
     }
 
-    private void PopulateEventDetails(){
+    private void PopulateEventDetails() {
         eventName.setText(event.getName());
         eventType.setText(event.getEventType().getName());
         eventDescription.setText(event.getDescription());
@@ -122,18 +181,21 @@ public class EventDetailsFragment extends Fragment {
 
         // Populate the organizer details
         GetOrganizerDTO organizer = event.getOrganizer();
-        eventOrganizerName.setText(organizer.getFirstName()+" "+organizer.getLastName());
-        eventOrganizerLocation.setText(organizer.getLocation().getCity()+", "+organizer.getLocation().getCountry());
+        eventOrganizerName.setText(organizer.getFirstName() + " " + organizer.getLastName());
+        eventOrganizerLocation.setText(organizer.getLocation().getCity() + ", " + organizer.getLocation().getCountry());
         eventOrganizerEmail.setText(organizer.getEmail());
         eventOrganizerPhone.setText(organizer.getPhoneNumber());
 
-        if(event.getOrganizer().getProfilePhoto()!=null)
+        if (event.getOrganizer().getProfilePhoto() != null)
             Picasso.get().load(event.getOrganizer().getProfilePhoto()).into(eventOrganizerProfilePhoto);
     }
 
-    private void PopulateAgenda()
-    {
+    private void PopulateAgenda() {
         agendaRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         agendaRecyclerView.setAdapter(new AgendaItemListAdapter((List<GetAgendaItemDTO>) agenda));
+    }
+
+    private void RefreshFavouriteButton() {
+        favouriteButton.setImageResource(isFavourite ? R.drawable.heart_filled : R.drawable.heart_outline);
     }
 }
