bb1b989fb51a7fd1c3508ef96f82e06fb799c663
Marina Ivanovic <ivanovic.sv6.2022@uns.ac.rs>
2024-12-29 19:00:07 +0100

    Add PaginatedViewModel for better modularity

--- EventPlanner/app/src/main/java/m3/eventplanner/fragments/PaginatedViewModel.java
+++ EventPlanner/app/src/main/java/m3/eventplanner/fragments/PaginatedViewModel.java
@@ -1,4 +1,33 @@
 package m3.eventplanner.fragments;
 
-public class PaginatedViewModel {
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import m3.eventplanner.models.PagedResponse;
+
+public abstract class PaginatedViewModel<T> extends ViewModel {
+    protected final MutableLiveData<PagedResponse<T>> pagedData = new MutableLiveData<>();
+    protected final MutableLiveData<String> error = new MutableLiveData<>();
+    protected int currentPage = 0;
+    protected int totalPages = 0;
+
+    public LiveData<PagedResponse<T>> getPagedData() {
+        return pagedData;
+    }
+
+    public LiveData<String> getError() {
+        return error;
+    }
+
+    public int getCurrentPage() {
+        return currentPage;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public abstract void fetchPage(int page);
 }
+
