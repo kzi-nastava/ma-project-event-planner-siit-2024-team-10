5bc293b04325035042b2b0d7dab7a9aa63482272
Mijat Krivokapić <mijat2004@gmail.com>
2024-12-30 18:25:20 +0100

    Add agenda item dtos

---
+++ EventPlanner/app/src/main/java/m3/eventplanner/models/CreateAgendaItemDTO.java
@@ -0,0 +1,62 @@
+package m3.eventplanner.models;
+
+import java.time.LocalTime;
+
+public class CreateAgendaItemDTO {
+    private String name;
+    private String description;
+    private String location;
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+    public CreateAgendaItemDTO() {
+    }
+
+    public CreateAgendaItemDTO(String name, String description, String location, LocalTime startTime, LocalTime endTime) {
+        this.name = name;
+        this.description = description;
+        this.location = location;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+}
---
+++ EventPlanner/app/src/main/java/m3/eventplanner/models/CreatedAgendaItemDTO.java
@@ -0,0 +1,72 @@
+package m3.eventplanner.models;
+
+import java.time.LocalTime;
+
+public class CreatedAgendaItemDTO {
+    private int id;
+    private String name;
+    private String description;
+    private String location;
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+    public CreatedAgendaItemDTO() {
+    }
+
+    public CreatedAgendaItemDTO(int id, String name, String description, String location, LocalTime startTime, LocalTime endTime) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.location = location;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+}
