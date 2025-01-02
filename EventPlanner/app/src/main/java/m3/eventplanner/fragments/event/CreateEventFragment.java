c9bc8fcaf24041c1914dcb762db6ff75e59386da
Mijat Krivokapić <mijat2004@gmail.com>
2025-01-02 17:45:27 +0100

    Implement event types dropdown

--- EventPlanner/app/src/main/java/m3/eventplanner/fragments/event/CreateEventFragment.java
+++ EventPlanner/app/src/main/java/m3/eventplanner/fragments/event/CreateEventFragment.java
@@ -2,65 +2,106 @@ package m3.eventplanner.fragments.event;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.List;
 
 import m3.eventplanner.R;
+import m3.eventplanner.clients.ClientUtils;
+import m3.eventplanner.databinding.FragmentCreateEventBinding;
+import m3.eventplanner.databinding.FragmentEventDetailsBinding;
+import m3.eventplanner.models.GetEventDTO;
+import m3.eventplanner.models.GetEventTypeDTO;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CreateEventFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class CreateEventFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FragmentCreateEventBinding binding;
+    private GetEventTypeDTO eventType;
+    private CreateEventViewModel viewModel;
+    private ClientUtils clientUtils;
 
     public CreateEventFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CreateEventFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CreateEventFragment newInstance(String param1, String param2) {
-        CreateEventFragment fragment = new CreateEventFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_event, container, false);
+        binding = FragmentCreateEventBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        viewModel = new ViewModelProvider(this).get(CreateEventViewModel.class);
+        clientUtils = new ClientUtils(requireContext());
+        viewModel.initialize(clientUtils);
+
+        viewModel.getEventTypes().observe(getViewLifecycleOwner(), this::setUpEventTypeSpinner);
+        viewModel.loadEventTypes();
+    }
+
+    private void setUpEventTypeSpinner(List<GetEventTypeDTO> eventTypes) {
+        Spinner eventTypeSpinner = binding.eventTypeSpinner;
+        ArrayAdapter<GetEventTypeDTO> adapter = getGetEventTypeDTOArrayAdapter(eventTypes);
+        eventTypeSpinner.setAdapter(adapter);
+        eventTypeSpinner.setTag(eventTypes);
+
+        eventTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (!eventTypes.isEmpty()) {
+                    GetEventTypeDTO selectedEventType = eventTypes.get(position);
+                    if (selectedEventType != null) {
+                        eventType=selectedEventType;
+                    } else {
+                    }
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    @NonNull
+    private ArrayAdapter<GetEventTypeDTO> getGetEventTypeDTOArrayAdapter(List<GetEventTypeDTO> eventTypes) {
+        ArrayAdapter<GetEventTypeDTO> adapter = new ArrayAdapter<GetEventTypeDTO>(requireContext(), android.R.layout.simple_spinner_item, eventTypes) {
+            @Override
+            public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView textView = (TextView) view;
+                textView.setText(eventTypes.get(position).getName());
+                return view;
+            }
+
+            @NonNull
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                TextView textView = (TextView) super.getView(position, convertView, parent);
+                textView.setText(eventTypes.get(position).getName());
+                return textView;
+            }
+        };
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        return adapter;
     }
 }
