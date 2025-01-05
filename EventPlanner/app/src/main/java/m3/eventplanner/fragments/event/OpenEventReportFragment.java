a88e26b4af9f99afcd52e33b1bdacf6bf04708af
Mijat Krivokapić <mijat2004@gmail.com>
2025-01-05 16:15:29 +0100

    Implement export report to pdf functionality

--- EventPlanner/app/src/main/java/m3/eventplanner/fragments/event/OpenEventReportFragment.java
+++ EventPlanner/app/src/main/java/m3/eventplanner/fragments/event/OpenEventReportFragment.java
@@ -2,65 +2,54 @@ package m3.eventplanner.fragments.event;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import m3.eventplanner.R;
+import m3.eventplanner.databinding.FragmentEventDetailsBinding;
+import m3.eventplanner.databinding.FragmentOpenEventReportBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link OpenEventReportFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class OpenEventReportFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FragmentOpenEventReportBinding binding;
+    private OpenEventReportViewModel viewModel;
 
     public OpenEventReportFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment OpenEventReportFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static OpenEventReportFragment newInstance(String param1, String param2) {
-        OpenEventReportFragment fragment = new OpenEventReportFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_open_event_report, container, false);
+        binding = FragmentOpenEventReportBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        if (getArguments() != null) {
+            int eventId = getArguments().getInt("selectedEventId");
+            viewModel = new ViewModelProvider(this).get(OpenEventReportViewModel.class);
+            viewModel.initialize(requireContext(),eventId);
+        }
+
+        binding.exportToPdfButton.setOnClickListener(v->{
+            viewModel.exportToPdf();
+        });
+
+        viewModel.getError().observe(getViewLifecycleOwner(), error ->
+                Toast.makeText(getContext(), error, Toast.LENGTH_SHORT).show()
+        );
+
+        viewModel.getSuccessMessage().observe(getViewLifecycleOwner(), message ->
+                Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show()
+        );
     }
 }
--- EventPlanner/app/src/main/java/m3/eventplanner/fragments/event/OpenEventReportViewModel.java
+++ EventPlanner/app/src/main/java/m3/eventplanner/fragments/event/OpenEventReportViewModel.java
@@ -1,6 +1,78 @@
 package m3.eventplanner.fragments.event;
 
+import static androidx.core.content.ContentProviderCompat.requireContext;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.io.File;
+import java.io.IOException;
+
+import m3.eventplanner.clients.ClientUtils;
+import m3.eventplanner.utils.PdfUtils;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class OpenEventReportViewModel extends ViewModel {
+    private Context context;
+    private ClientUtils clientUtils;
+    private PdfUtils pdfUtils;
+    private int eventId;
+    public MutableLiveData<String> error=new MutableLiveData<>();
+    public MutableLiveData<String> successMessage=new MutableLiveData<>();
+
+    public void initialize(Context context, int eventId){
+        this.eventId = eventId;
+        this.context = context;
+        this.clientUtils = new ClientUtils(context);
+        this.pdfUtils = new PdfUtils(context);
+    }
+
+    public LiveData<String> getError() {
+        return error;
+    }
+
+    public LiveData<String> getSuccessMessage() {
+        return successMessage;
+    }
+
+    public void exportToPdf(){
+        clientUtils.getEventService().getOpenEventReport(eventId).enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    new Thread(() -> {
+                        try {
+                            // Read bytes from ResponseBody
+                            byte[] pdfBytes = response.body().bytes();
+
+                            // Save the PDF file
+                            File file = pdfUtils.savePdfFile(pdfBytes, "open_event_report");
+
+                            // Open the saved PDF file on the main thread
+                            new Handler(Looper.getMainLooper()).post(() -> {
+                                pdfUtils.openPdfFile(file);
+                            });
+                        } catch (IOException e) {
+                            error.setValue("Error saving PDF report");
+                        }
+                    }).start();
+                } else {
+                    error.setValue("Error generating PDF report");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                error.setValue("Error generating PDF report");
+            }
+        });
+    }
 }
