5c833f05243538c2d9f35e621ae82e33edb1565c
Mijat Krivokapić <mijat2004@gmail.com>
2025-01-05 16:36:15 +0100

    Add charting dependencies

--- EventPlanner/app/build.gradle.kts
+++ EventPlanner/app/build.gradle.kts
@@ -73,4 +73,5 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-livedata:2.6.1")
     implementation("androidx.lifecycle:lifecycle-runtime:2.6.1")
     implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 }
