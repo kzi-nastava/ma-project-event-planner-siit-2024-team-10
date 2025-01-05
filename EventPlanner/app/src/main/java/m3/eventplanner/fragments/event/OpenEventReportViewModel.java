86f8fff7471a64eb2fed72ccba4f711aa0fbfa2e
Mijat Krivokapić <mijat2004@gmail.com>
2025-01-05 18:57:09 +0100

    Implement event stats fetching

--- EventPlanner/app/src/main/java/m3/eventplanner/fragments/event/OpenEventReportViewModel.java
+++ EventPlanner/app/src/main/java/m3/eventplanner/fragments/event/OpenEventReportViewModel.java
@@ -14,6 +14,8 @@ import java.io.File;
 import java.io.IOException;
 
 import m3.eventplanner.clients.ClientUtils;
+import m3.eventplanner.models.GetEventDTO;
+import m3.eventplanner.models.GetEventStatsDTO;
 import m3.eventplanner.utils.PdfUtils;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -27,6 +29,7 @@ public class OpenEventReportViewModel extends ViewModel {
     private int eventId;
     public MutableLiveData<String> error=new MutableLiveData<>();
     public MutableLiveData<String> successMessage=new MutableLiveData<>();
+    public MutableLiveData<GetEventStatsDTO> eventStats = new MutableLiveData<>();
 
     public void initialize(Context context, int eventId){
         this.eventId = eventId;
@@ -41,6 +44,28 @@ public class OpenEventReportViewModel extends ViewModel {
 
     public LiveData<String> getSuccessMessage() {
         return successMessage;
+    }
+
+    public LiveData<GetEventStatsDTO> getEventStats() {
+        return eventStats;
+    }
+
+    public void loadEventStats(){
+        clientUtils.getEventService().getEventStats(eventId).enqueue(new Callback<GetEventStatsDTO>() {
+            @Override
+            public void onResponse(Call<GetEventStatsDTO> call, Response<GetEventStatsDTO> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    eventStats.setValue(response.body());
+                } else {
+                    error.setValue("Failed to load event stats");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<GetEventStatsDTO> call, Throwable t) {
+                error.setValue(t.getMessage() != null ? t.getMessage() : "Error loading event stats");
+            }
+        });
     }
 
     public void exportToPdf(){
