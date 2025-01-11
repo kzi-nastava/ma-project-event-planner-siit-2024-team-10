5da5cb7390e14d7e2c17cb15f050640f34b1ac07
milicaradicc <milica.t.radic@gmail.com>
2025-01-11 13:17:46 +0100

    Add category service

--- EventPlanner/app/src/main/java/m3/eventplanner/clients/CategoryService.java
+++ EventPlanner/app/src/main/java/m3/eventplanner/clients/CategoryService.java
@@ -2,10 +2,18 @@ package m3.eventplanner.clients;
 
 import java.util.Collection;
 
+import m3.eventplanner.models.CreatedEventTypeDTO;
 import m3.eventplanner.models.GetOfferingCategoryDTO;
+import m3.eventplanner.models.UpdateCategoryDTO;
+import m3.eventplanner.models.UpdatedEventTypeDTO;
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface CategoryService {
     @Headers({
@@ -14,4 +22,15 @@ public interface CategoryService {
     })
     @GET("categories")
     Call<Collection<GetOfferingCategoryDTO>> getCategories();
+    @POST("categories")
+    Call<CreatedEventTypeDTO> addCategory(@Body CreateCategoryDTO categoryDTO);
+
+    @PUT("categories/{id}")
+    Call<UpdatedEventTypeDTO> editCategory(@Path("id") int id, @Body UpdateCategoryDTO categoryDTO);
+
+    @DELETE("categories/{id}")
+    Call<Void> deleteCategory(@Path("id") int id);
+    @PUT("categories/{id}/approve")
+    Call<Void> approve(@Path("id") int id);
+
 }
