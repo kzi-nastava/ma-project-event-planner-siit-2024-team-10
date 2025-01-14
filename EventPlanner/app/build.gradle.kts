9d6cccc32826c65633dee3ce06ef654a0dd288b9
milicaradicc <milica.t.radic@gmail.com>
2025-01-14 14:46:46 +0100

    Update navigation

--- EventPlanner/app/build.gradle.kts
+++ EventPlanner/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
 
     defaultConfig {
         applicationId = "m3.eventplanner"
-        minSdk = 30
+        minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
