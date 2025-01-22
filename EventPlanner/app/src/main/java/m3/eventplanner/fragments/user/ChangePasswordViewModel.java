4cb8c5d23951b80a3353890e6fc55ee01e1fcd5a
Mijat Krivokapić <mijat2004@gmail.com>
2025-01-22 15:00:37 +0100

    Implement change password logic

--- EventPlanner/app/src/main/java/m3/eventplanner/fragments/user/ChangePasswordViewModel.java
+++ EventPlanner/app/src/main/java/m3/eventplanner/fragments/user/ChangePasswordViewModel.java
@@ -4,9 +4,15 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.io.IOException;
+
 import m3.eventplanner.auth.TokenManager;
 import m3.eventplanner.clients.ClientUtils;
+import m3.eventplanner.models.ChangePasswordDTO;
 import m3.eventplanner.models.GetUserDTO;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ChangePasswordViewModel extends ViewModel {
     private ClientUtils clientUtils;
@@ -27,5 +33,26 @@ public class ChangePasswordViewModel extends ViewModel {
         this.tokenManager=tokenManager;
     }
 
+    public void changePassword(ChangePasswordDTO changePasswordDTO){
+        clientUtils.getUserService().changePassword(tokenManager.getAccountId(),changePasswordDTO).enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    successMessage.setValue("Password changed successfully!");
+                } else {
+                    try {
+                        error.setValue(response.errorBody().string());
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                error.setValue(t.getMessage());
+            }
+        });
+    }
 
 }
